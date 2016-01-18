aa0148106bf284f093fb4b1f0ef9f97ec24dee70
Patrick Arlt <patrick.arlt@gmail.com>
2016-01-18 12:46:15 -0800

    reproduce source map bug

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
-import babel from 'rollup-plugin-babel';
+import npm from 'rollup-plugin-npm';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'lib/index.js',
-  sourceMap: true,
-  plugins: [babel()]
+  format: 'umd',
+  dest: 'dist/rollup-starter-project.js',
+  sourceMap: 'dist/rollup-starter-project.js.map',
+  moduleName: 'rollupStarterProject',
+  plugins: [
+    npm({
+      jsnext: true
+    }),
+    uglify()
+  ]
 };
